7f1d66aeefbf3b9cfef8dff76f5471a9a589eb82
vstavskyi <vstavskyi@users.noreply.github.com>
2018-09-10 15:42:14 +0300

    declaring output parameters

--- src/jamdb_oracle_tns_decoder.erl
+++ src/jamdb_oracle_tns_decoder.erl
@@ -166,16 +166,15 @@ decode_token(iov, Data, {Ver, RowFormat, Type}) when is_atom(Type) ->
 	    decode_token(Rest3, {Ver, [decode_param(B) || B <- Bind], Type})
     end;
 decode_token(rxd, Data, {Ver, _RowFormat, fetch}) ->
-    {Rest2, RowFormat} = decode_token(dcb, decode_next(ub1,Data), Ver),
+    {Rest2, CursorRowFormat} = decode_token(dcb, decode_next(ub1,Data), Ver),
     Cursor = decode_ub4(Rest2),
     Rest3 = decode_next(ub4,Rest2),
-    Rest4 = decode_next(ub2,Rest3),
-    decode_token(Rest4, {Cursor, RowFormat, []});                    %%fetch cursor
+    {{Cursor, CursorRowFormat}, decode_next(ub2,Rest3)};
 decode_token(rxd, Data, {Ver, RowFormat, Type}) when is_atom(Type) ->
-    try decode_data(Data, RowFormat, [], Type) of
+    case decode_data(Data, [], {[], RowFormat, Ver, Type}) of
+    	{{Cursor, CursorRowFormat}, Rest2} ->
+	    decode_token(Rest2, {Cursor, CursorRowFormat, []});      %%fetch cursor
         {Rows, Rest2} -> decode_token(Rest2, {0, RowFormat, Rows})   %%proc_result
-    catch
-        error:_ -> decode_token(rxd, Data, {Ver, [], fetch})         %%fetch cursor
     end;
 decode_token(rxd, Data, {Cursor, RowFormat, Bvc, Rows}) ->
     LastRow = last(Rows),
@@ -368,19 +367,22 @@ decode_bvc(Data, Acc, Num, I) when is_list(Acc) ->
     Bvc = decode_bvc(decode_ub1(Data), {}, 0, I),
     decode_bvc(decode_next(ub1,Data), Acc++tuple_to_list(Bvc), Num-1, I+1).
 
-decode_data(Data, [], Values, _Type) ->
+decode_data(Data, Values, {[], [], _Ver, _Type}) ->
     {lists:reverse(Values), Data};
-decode_data(Data, [#format{param=in}|RestRowFormat], Values, Type) ->
-    decode_data(Data, RestRowFormat, Values, Type);
-decode_data(_Data, [#format{data_type=?TNS_TYPE_REFCURSOR}|_RestRowFormat], _Values, _Type) ->
-    erlang:error(cursor);
-decode_data(Data, [ValueFormat|RestRowFormat], Values, Type=return) ->
+decode_data(Data, _Values, {DefCol, [], _Ver, _Type}) ->
+    {DefCol, Data};
+decode_data(Data, Values, {DefCol, [#format{param=in}|RestRowFormat], Ver, Type}) ->
+    decode_data(Data, Values, {DefCol, RestRowFormat, Ver, Type});
+decode_data(Data, Values, {_DefCol, [#format{data_type=?TNS_TYPE_REFCURSOR}|RestRowFormat], Ver, Type}) ->
+    {DefCol, Rest2} = decode_token(rxd, Data, {Ver, [], fetch}),
+    decode_data(Rest2, Values, {DefCol, RestRowFormat, Ver, Type});
+decode_data(Data, Values, {DefCol, [ValueFormat|RestRowFormat], Ver, Type=return}) ->
     Num = decode_ub4(Data),
     {Value, RestData} = decode_data(decode_next(ub4,Data), ValueFormat, [], Num, Type),
-    decode_data(RestData, RestRowFormat, [Value|Values], Type);    
-decode_data(Data, [ValueFormat|RestRowFormat], Values, Type=block) ->
+    decode_data(RestData, [Value|Values], {DefCol, RestRowFormat, Ver, Type});
+decode_data(Data, Values, {DefCol, [ValueFormat|RestRowFormat], Ver, Type=block}) ->
     {Value, RestData} = decode_data(Data, ValueFormat),
-    decode_data(decode_next(ub2,RestData), RestRowFormat, [Value|Values], Type).
+    decode_data(decode_next(ub2,RestData), [Value|Values], {DefCol, RestRowFormat, Ver, Type}).
 
 decode_data(Data, _ValueFormat, Values, 0, _Type) ->
     {lists:reverse(Values), Data};
@@ -712,4 +714,5 @@ decode_long(Data) ->
     Rest2 = decode_next(chr,Data),
     {Value, decode_next(ub2,Rest2,2)}.
 
+decode_helper(param, Data) -> decode_token(oac, ?ENCODER:encode_token(oac, Data));
 decode_helper(tz, Data) -> ltz(Data).
